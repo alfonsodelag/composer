787ad005886efe10e7ae0799532c4cdca460f737
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-02-06 14:43:28 +0100

    changes to index and composer.json

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
-use Guzzlehttp\Client;
+require 'vendor/autoload.php';
 
-$client = new Client([
-    'base_uri' => 'http//httpbin.org'
 
-    'timeout' => 2.0
+$client = new \GuzzleHttp\Client();
+$res = $client->request('GET', 'https://itunes.apple.com/lookup?isbn=9780316069359', [
+    'auth' => ['user', 'pass']
 ]);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type')[0];
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...'
+
+// Send an asynchronous request.
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed! ' . $response->getBody();
+});
+$promise->wait();
 
 ?>
